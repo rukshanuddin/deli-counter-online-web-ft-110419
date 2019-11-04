90b503ba6dd2826ef85691c04f12da5458abda96
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-04 19:57:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,6 @@ end
 def take_a_number(katz_deli, name)
   katz_deli << name
   line = []
-  katz_deli.each_with_index {|person, idx| line << "Welcome, #{person}. You are number #{idx+1} in line." puts line[idx]}
-  
+  katz_deli.each_with_index {|person, idx| line << "Welcome, #{person}. You are number #{idx+1} in line."}
+  puts line[idx]
 end
