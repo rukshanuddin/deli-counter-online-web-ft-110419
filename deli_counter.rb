a785f4c3c3f47b01a9eedc96a02d09e134db06a4
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-04 19:34:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     line = []
     katz_deli.each_with_index {|person, idx| line << "#{idx + 1}. #{person}"}
     line.join(" ")
-    "The line is currently: " + line
+    "The line is currently: " + line[0]
   end
 
 end
