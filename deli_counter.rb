6b9cd68680901af1e23669c738f2e8168b33cc26
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-04 19:58:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
-def line(katz_deli)
-  if katz_deli.length == 0
+def line(deli)
+  if deli.length == 0
     puts "The line is currently empty."
-  elsif katz_deli.length > 0
+  elsif deli.length > 0
     line = []
-    katz_deli.each_with_index {|person, idx| line << "#{idx + 1}. #{person}"}
+    deli.each_with_index {|person, idx| line << "#{idx + 1}. #{person}"}
     puts "The line is currently: " + line.join(" ")
   end
 end
